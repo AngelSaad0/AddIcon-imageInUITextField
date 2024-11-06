6a993a20d3549edd96d76cccc63720e5f4cc8952
AngelSaad0 <engysaad573new@gmail.com>
2024-11-06 03:34:09 +0200

    added image In UITextField And added Single Line to UITextField

--- imageInUITextFieldAndSingleLine/ViewController.swift
+++ imageInUITextFieldAndSingleLine/ViewController.swift
@@ -18,15 +18,26 @@ class ViewController: UIViewController {
     }
 
     private func setupUI() {
+        let nameFrame = nameTx.frame
+        let emailFrame = emailTx.frame
+        let passwordFrame = emailTx.frame
+
         nameTx.setupLeftImage(imageName: "person")
         emailTx.setupLeftImage(imageName: "envelope")
         paswordTx.setupLeftImage(imageName: "eye.slash")
+
+        nameTx.addBottomLine(frame: CGRect(x: 0, y: nameFrame.height, width: nameFrame.width, height: 1.4))
+        emailTx.addBottomLine(frame: CGRect(x: 0, y: emailFrame.height, width: emailFrame.width, height: 2.5))
+        paswordTx.addBottomLine(frame: CGRect(x: 0, y: passwordFrame.height, width: passwordFrame.width, height: 3))
+
     }
 
 }
 
 
+
 extension UITextField {
+
     func setupLeftImage(imageName: String) {
 
         let imageView = UIImageView(frame: CGRect(x: 10, y: 10, width: 20, height: 20))
@@ -40,6 +51,17 @@ extension UITextField {
         leftViewMode = .always
         leftView?.tintColor = .systemGray
     }
+
+    func addBottomLine(frame: CGRect, color: UIColor = .systemGray) {
+
+        let bottomline = CALayer()
+        bottomline.frame = frame
+        bottomline.backgroundColor = UIColor.brown.cgColor
+        self.borderStyle = .none
+        self.layer.addSublayer(bottomline)
+
+    }
+
 }
 
 
